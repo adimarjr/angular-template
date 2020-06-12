7bddd9b29a6aa9e94ae0a5266dd35a94c54211da
Adimar Jr <adimarjr@gmail.com>
2020-06-12 11:50:17 -0300

    added item component for tests

---
+++ angular-template/src/app/test/test-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ItemComponent } from './components/item/item.component';
+
+
+const routes: Routes = [
+  { path: '', component: ItemComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TestRoutingModule { }
